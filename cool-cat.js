0b27613252360456706c6fe1569ad902edc121a3
Apple <apple@Apples-MacBook-Pro-3.local>
2015-10-15 09:05:03 -0700

    created a chart using Chart.js and voting data

--- cool-cat.js
+++ cool-cat.js
@@ -1,7 +1,11 @@
+var data = [];
+var myChart;
+
 var Cat = function(path, catName) {
 	this.path = path;
-	this.catName = catName;
+	this.name = catName;
 	this.votes = 0;
+	data.push({value: 1, label: catName, color: '#CCCCFF', highlight: '#FFCCFF'}); 
 };
 
 var listCreateCat = [
@@ -28,31 +32,66 @@ var randNumber = function() {
 var rand1 = randNumber();
 var rand2 = randNumber();
 
+var cat1header = document.getElementById("cat1header");
+var car2header = document.getElementById("cat2header");
+
 function displayPhotos() {
 	get1.src = listCreateCat[rand1].path;
 	get2.src = listCreateCat[rand2].path;
 	console.log(listCreateCat[rand1].path + 'is Cat 1');
 	console.log(listCreateCat[rand2].path + ' is Cat 2');
-	// get1.appendChild(listCreateCat[rand1].path);
-	// get2.appendChild(listCreateCat[rand2].path);	
+	cat1header.innerHTML = listCreateCat[rand1].name;
+	cat2header.innerHTML = listCreateCat[rand2].name;
 }; 
 
 var get1 = document.getElementById("randomImage1");
 var get2 = document.getElementById("randomImage2");
 
+//stopping duplicates from happening
+var randPhoto = function (){
+    while (rand1 === rand2) {
+      rand1 = randNumber();
+  	}
+};
+
 displayPhotos(); // when page loads
-var totalVotes = [];
 
 get1.addEventListener('click', function() {
-	console.log(listCreateCat[rand1].catName + " was clicked");
+	console.log(cat1header.innerHTML + " was clicked");
 	listCreateCat[rand1].votes +=1; 
-	console.log(listCreateCat[rand1].catName + " has " + listCreateCat[rand1].votes + " votes");
+	console.log(cat1header.innerHTML + " has " + listCreateCat[rand1].votes + " votes");
+	for (var i in data) {
+		if (data[i].label === cat1header.innerHTML) {
+			data[i].value += 1;
+		}
+	}
+	for (var i in myChart.segments) {
+		if (myChart.segments[i].label === cat1header.innerHTML) {
+			myChart.segments[i].value += 1;
+		}
+	}
+	myChart.update();
+	randPhoto();
+	now.style.visibility = 'visible';
 });
 
 get2.addEventListener('click', function() {
-	console.log(listCreateCat[rand2].catName + " was clicked");
+	console.log(cat2header.innerHTML + " was clicked");
 	listCreateCat[rand2].votes +=1;
-	console.log(listCreateCat[rand2].catName + " has " + listCreateCat[rand2].votes + " votes");
+	console.log(cat2header.innerHTML+ " has " + listCreateCat[rand2].votes + " votes");
+	for (var i in data) {
+		if (data[i].label === cat2header.innerHTML) {
+			data[i].value += 1;
+		}
+	}
+	for (var i in myChart.segments) {
+		if (myChart.segments[i].label === cat2header.innerHTML) {
+			myChart.segments[i].value += 1;
+		}
+	}
+	myChart.update();
+	randPhoto()
+	now.style.visibility = 'visible';
 });
 
 var now = document.getElementById('new-cat');
@@ -61,9 +100,21 @@ now.addEventListener('click', function(){
 	rand2 = randNumber();
 	displayPhotos();
 	console.log("Click my button");
+	now.style.visibility = 'hidden';
 });
 
-//duplicate images
-//multiple votes on images
+function findLabel(array, label) {
+	for(var i in array){
+		if (array[i].label === label){
+		return array[i];
+		}
+	}
+};
 
-
+var context = document.getElementById('bestCat').getContext('2d');
+myChart = new Chart(context).Doughnut(data, {
+	animationSteps: 100,
+	animationEasing: "easeOutBounce",
+	animateRotate: true,
+	animateScale: true,
+});
